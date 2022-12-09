7d4dabe1d9e296417107369e4f6dd85ba18feb53
Andrea Cecchini <cecchiniandrea.lavoro@gmail.com>
2022-12-09 15:06:45 +0100

    Base main file for testing pthreads

---
+++ c_source/main.c
@@ -0,0 +1,38 @@
+/**
+ * @file main.c
+ * @author Andrea Cecchini (andrea.cecchini13@studio.unibo.it)
+ * @brief 
+ * @version 0.1
+ * @date 2022-12-09
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+/*
+ * We can avoid defining this symbols while adding them when compiling 
+ * inside compiler options -D...
+ * 
+ * #define _THREAD_SAFE
+ */
+
+/* INCLUDE SECTION */
+#include <stdlib.h>
+#include <pthread.h>
+
+
+/**
+ * @brief Entry Point of the program 
+ * 
+ * @param argc Number of argument passed through command line  
+ * @param argv Arguements passed through command line 
+ * @return int Exit-status of the program. 
+ */
+int main(int argc, char const *argv[])
+{
+    /* [TODO] */
+    return (EXIT_SUCCESS);
+}
+
+
+
